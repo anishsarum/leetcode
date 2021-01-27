93aa28c28f74e85db62fe1c66b5b899618bd272c
Anish Thapa <anishsarum@protonmail.com>
2021-01-27 00:07:11 +0000

    Update reverseInteger.py

--- reverseInteger.py
+++ reverseInteger.py
@@ -5,5 +5,18 @@ class Solution:
         
         for i in range(len(xStr)-1,-1,-1):
             new += xStr[i]
+        
+        if x < 0:
+            filt = ""
             
-        return new
+            for i in range(len(new)-1):
+                filt += new[i]
+                
+            new = - int(filt)
+        
+        new = int(new)
+        
+        if new >= (-2 ** 31) and new <= (2 ** 31 - 1):
+            return new
+        
+        return 0
